6a5a2198b21c633b81f46f26465eda4df7eb8da6
Hindun <32257792+Hindun@users.noreply.github.com>
2018-03-27 18:40:35 +0800

    Membuat Atribut dan Behavior

--- src/hindun/Rok.java
+++ src/hindun/Rok.java
@@ -10,5 +10,18 @@ package hindun;
  * @author ACER
  */
 public class Rok {
+    String Bahan;
+    String Merk;
+    String Ukuran;
+    String Warna;
+    String Motif;
     
+    void tampildata(){
+        System.out.println("Bahan   = "+Bahan);
+        System.out.println("Merk    = "+Merk);
+        System.out.println("Ukuran  = "+Ukuran);
+        System.out.println("Warna   = "+Warna);
+        System.out.println("Motif   = "+Motif);
+        System.out.println("------------------");
+    }
 }
